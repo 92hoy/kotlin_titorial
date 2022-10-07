84729488a1d95c759165c8ee1a4b7b5b2b0393ba
92hoy <ho_9209@naver.com>
2022-10-07 16:44:57 +0900

    Feat: ListView

--- app/src/main/java/com/example/kotlin_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_tutorial/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -33,52 +37,78 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //----------  리 스 트 뷰 ------------------
+        setContent {
+            LazyColumn {
+                itemsIndexed(
+                    arrayOf("This", "is", "Jetpack", "Compose", "I", "Learned")
+                ) { index, item ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        elevation = 5.dp,
+                        shape = RoundedCornerShape(15.dp)
+                    ) {
+                        Text(
+                            text = "$item",
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 24.dp)
+                        )
+                    }
+                }
+            }
+        }
 
         //---------- Text font 적용 및 꾸미기------------------
         // res/font 에 폰트 넣기(*Convention 유의 (소문자 & 언더바 _ ))
-        val fontFamily = FontFamily(
-            Font(R.font.indieflower_regular, FontWeight.Medium),
-        )
-        setContent {
+//        val fontFamily = FontFamily(
+//            Font(R.font.indieflower_regular, FontWeight.Medium),
+//        )
+//        setContent {
+//
+//            Box(
+//                Modifier
+//                    .fillMaxSize()
+//                    .background(Color(0xFF101010))
+//            ) {
+//                Text(
+//                    text = buildAnnotatedString {
+//                        withStyle(
+//                            style = SpanStyle(
+//                                Color.Green,
+//                                fontSize = 50.sp
+//                            ),
+//                        ) {
+//                            append("J")
+//                        }
+//                        append("etpack ")
+//
+//                        withStyle(
+//                            style = SpanStyle(
+//                                Color.Green,
+//                                fontSize = 50.sp
+//                            )
+//                        ) {
+//                            append("C")
+//                        }
+//                        append("ompose")
+//                    },
+//                    textAlign = TextAlign.Center,
+//                    fontFamily = fontFamily,
+//                    fontSize = 30.sp,
+//                    color = Color.White,
+//                    fontStyle = FontStyle.Italic,
+//                    textDecoration = TextDecoration.Underline
+//
+//                )
+//            }
 
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF101010))
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                Color.Green,
-                                fontSize = 50.sp
-                            ),
-                        ) {
-                            append("J")
-                        }
-                        append("etpack ")
-
-                        withStyle(
-                            style = SpanStyle(
-                                Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    textAlign = TextAlign.Center,
-                    fontFamily = fontFamily,
-                    fontSize = 30.sp,
-                    color = Color.White,
-                    fontStyle = FontStyle.Italic,
-                    textDecoration = TextDecoration.Underline
-
-                )
-            }
-
-            //---------- 이미지 + 카드 나열 ------------------
+        //---------- 이미지 + 카드 나열 ------------------
 //            val painter = painterResource(id = R.drawable.kermit)
 //            val description = "Kermit in the snow"
 //            val title = "Kermit is playing in the snow"
@@ -171,50 +201,50 @@ class MainActivity : ComponentActivity() {
 //                    }
 //                }
 //            }
-        }
     }
+}
 
-    @Composable
-    fun ImageCard(
-        painter: Painter,
-        contentDescription: String,
-        title: String,
-        modifier: Modifier = Modifier
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Card(
-            modifier = modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(15.dp),
-            elevation = 5.dp
-        ) {
-            Box(modifier = modifier.height(200.dp)) {
-                Image(
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    contentScale = ContentScale.Crop
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                            Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black,
-                                ),
-                                startY = 300f
-                            )
+        Box(modifier = modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black,
+                            ),
+                            startY = 300f
                         )
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(12.dp),
-                    contentAlignment = Alignment.BottomStart
-                ) {
-                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-                }
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
         }
     }
 }
+
 
