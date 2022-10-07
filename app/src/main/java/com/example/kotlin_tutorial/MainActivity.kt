ce39492f8125c135c5aa034385c54a0b46ee6b18
92hoy <ho_9209@naver.com>
2022-10-07 15:18:45 +0900

    Feat: 이미지카드 사용해보기

--- app/src/main/java/com/example/kotlin_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_tutorial/MainActivity.kt
@@ -3,41 +3,164 @@ package com.example.kotlin_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_tutorial.ui.theme.Kotlin_tutorialTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Kotlin_tutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            val painter = painterResource(id = R.drawable.kermit)
+            val description = "Kermit in the snow"
+            val title = "Kermit is playing in the snow"
+            Column(Modifier.background(Color.Cyan)) {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                )
+                {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+                }
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                )
+                {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+                }
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                )
+                {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        ImageCard(
+                            painter = painter,
+                            contentDescription = description,
+                            title = title
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp
+        ) {
+            Box(modifier = modifier.height(200.dp)) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black,
+                                ),
+                                startY = 300f
+                            )
+                        )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
                 ) {
-                    Greeting("Android")
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Kotlin_tutorialTheme {
-        Greeting("Android")
-    }
-}
